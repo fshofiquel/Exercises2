923651c8cf29a86fcbfdc7f41839dc0e1ecc0d90
fshofiquel <fshofique@gmail.com>
2021-06-11 19:27:03 -0400

    Initial commit

---
+++ src/main/java/ex24/Base.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Fazlur Shofiquel
+ */
+package ex24;
+
+import java.util.Scanner;
+
+public class Base
+{
+    public static void main(String[] args)
+    {
+        String wordOne;
+        String wordTwo;
+        Scanner input = new Scanner(System.in);
+
+
+    }
+
+    public static String isString(String wordOne, String wordTwo)
+    {
+        return null;
+    }
+
+}
