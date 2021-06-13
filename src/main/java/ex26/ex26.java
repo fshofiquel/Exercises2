bcaff4006279de70b78bbda2587e0bb98092ef95
fshofiquel <fshofique@gmail.com>
2021-06-12 21:35:40 -0400

    NUMBER 25 IS STILL NOT WORKING PROPERLY

--- src/main/java/ex26/ex26.java
+++ src/main/java/ex26/ex26.java
@@ -4,6 +4,51 @@
  */
 package ex26;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ex26
 {
+    public static void main(String[] args)
+    {
+        ex26 myEx26 = new ex26();
+        double n;  // num of months
+        double APR;
+        float b;  // balance
+        float p;  // monthly payment
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your balance? ");
+        b = input.nextFloat();
+        System.out.print("What is the APR on the card (as a percent)? ");
+        APR = input.nextInt();
+        System.out.print("What is the monthly payment you can make? ");
+        p = input.nextFloat();
+
+        n = myEx26.calculateMonthsUntilPaidOff(b, APR, p);
+
+        System.out.printf("It will take you %.0f months to pay off this card.", n);
+    }
+
+    public double calculateMonthsUntilPaidOff(float b, double APR, float p)
+    {
+        double n;
+        double i;
+        double aprInPer;
+        b = (float) Math.ceil(b);
+        p = (float) Math.ceil(p);
+
+
+        aprInPer = APR / 100;
+        i = aprInPer / 365;
+
+        n = -(1.0 / 30) * (Math.log(1 + b / p * (1 - Math.pow((1 + i), 30))) / Math.log(1 + i));
+        n = Math.ceil(n);
+
+        return n;
+    }
+
+
 }
---
+++ src/main/java/ex27/ex27.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Fazlur Shofiquel
+ */
+package ex27;
+
+public class ex27
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
